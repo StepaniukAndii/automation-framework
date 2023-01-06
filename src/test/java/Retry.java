60a0e7bc40ecb0668df0b03671d09131a1cd4681
TrofDanil <trofimov.danil.sergiovich@gmail.com>
2023-01-06 11:22:59 +0200

    Implement retry

---
+++ src/test/java/Retry.java
@@ -0,0 +1,17 @@
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+    int initCount = 0;
+    int maxRetryCount = 1;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if(initCount < maxRetryCount)
+        {
+            initCount++;
+            return true;
+        }
+        return false;
+    }
+}
