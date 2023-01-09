6671146b92c4a9fb7e3d51301c0932c9be248317
TrofDanil <trofimov.danil.sergiovich@gmail.com>
2023-01-09 19:21:54 +0200

    Add logger

--- src/test/java/Retry.java
+++ src/test/java/Retry.java
@@ -1,17 +1,24 @@
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
+import org.testng.log4testng.Logger;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 public class Retry implements IRetryAnalyzer {
+
     int initCount = 0;
     int maxRetryCount = 1;
 
     @Override
     public boolean retry(ITestResult result) {
-        if(initCount < maxRetryCount)
-        {
+        if (initCount < maxRetryCount) {
             initCount++;
+            Log4j.error("Test " + result.getTestName() + " failed. Start attempt " + initCount);
             return true;
         }
         return false;
     }
 }
+
+// to implement retry, write @Test(retryAnalyzer = Retry.class) before test
