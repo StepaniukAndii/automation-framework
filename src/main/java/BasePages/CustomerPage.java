0acdaa9376990cd5a230409eb8df5bc1c923cbc1
TrofDanil <trofimov.danil.sergiovich@gmail.com>
2023-01-16 12:41:42 +0200

    Init customer page

---
+++ src/main/java/BasePages/CustomerPage.java
@@ -0,0 +1,11 @@
+package BasePages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CustomerPage extends AbstractBasePage {
+
+    protected CustomerPage(WebDriver driver) {
+        super(driver);
+    }
+    
+}
