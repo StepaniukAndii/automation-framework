639ae68abf0e1ec9cdf1969f2ffd3094c18674e9
TrofDanil <trofimov.danil.sergiovich@gmail.com>
2023-01-17 14:02:41 +0200

    added elements

--- src/main/java/BasePages/CustomerPage.java
+++ src/main/java/BasePages/CustomerPage.java
@@ -1,6 +1,7 @@
 package BasePages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CustomerPage extends AbstractBasePage {
 
@@ -8,4 +9,31 @@ public class CustomerPage extends AbstractBasePage {
         super(driver);
     }
     
+    public WebElement TransactionButton() {
+        return $x("//button[@ng-class='btnClass1']");
+    }
+    public WebElement DepositButton() {
+        return $x("//button[@ng-class='btnClass2']");
+    }
+    public WebElement WithdrawButton() {
+        return $x("//button[@ng-class='btnClass3']");
+    }
+    public WebElement LogoutButton() {
+        return $x("//button[@class='logout']");
+    }
+    public WebElement Name(String name) {
+        return $x("//span[text()='" + name + "']");
+    }
+    public WebElement AccountNumber(String value) {
+        return $x("//strong[text()='" + value + "']");
+    }
+    public WebElement AccountBalance(String value) {
+        return $x("//strong[text()='" + value + "']");
+    }
+    public WebElement AccountCurrency(String value) {
+        return $x("//strong[text()='" + value + "']");
+    }
+    public WebElement AccountSelectField() {
+        return $x("//select[@id='accountSelect']");
+    }
 }
