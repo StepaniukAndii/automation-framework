3d72ed60ca0b1c8a6ad988aa337921a7c05d1fbc
Alexandr Yaitskyi <yaitskyi.al@gmail.com>
2023-01-21 10:49:39 +0200

    Edit AF57ManagerOpenAccountPage

--- src/main/java/pages/ManagerOpenAccountPage.java
+++ src/main/java/pages/ManagerOpenAccountPage.java
@@ -6,16 +6,27 @@ import org.openqa.selenium.WebElement;
 
 public class ManagerOpenAccountPage extends AbstractBasePage {
 
+
     public ManagerOpenAccountPage(WebDriver driver) {
         super(driver);
     }
 
+    private final static String SELECT_CUSTOMER = "//select[@name='userSelect']";
+    private final static String SELECT_CURRENCY = "//select[@name='currency']";
     private final static String PROGRESS_BUTTON = "//button[text()='Process']";
 
     @Override
     public ManagerOpenAccountPage open() {
         openUrl(env);
         return this;
+    }
+
+    public WebElement selectCustomer() {
+        return waitVisibleOfElement(SELECT_CUSTOMER);
+    }
+
+    public WebElement selectCurrency() {
+        return waitVisibleOfElement(SELECT_CURRENCY);
     }
 
     public WebElement Customer(int customerName) {
@@ -31,13 +42,6 @@ public class ManagerOpenAccountPage extends AbstractBasePage {
     }
 
     //HELP METHODS
-
-    public void alertMessagePrint() {
-        String alertMessage = driver.switchTo().alert().getText();
-        String[] splitted = alertMessage.split(":");
-        String account = splitted[1];
-        System.out.println(account);
-    }
 
     public ManagerOpenAccountPage selectCustomerName(int customerName) {
         Customer(customerName).click();
@@ -65,7 +69,13 @@ public class ManagerOpenAccountPage extends AbstractBasePage {
                 .clickOpenAccountsBtn();
         return new ManagerOpenAccountPage(driver);
 
+    }
 
+    public void alertMessagePrint() {
+        String alertMessage = driver.switchTo().alert().getText();
+        String[] splitted = alertMessage.split(":");
+        String account = splitted[1];
+        System.out.println(account);
     }
 
 }
