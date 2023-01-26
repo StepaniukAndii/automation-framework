8c7220482b72101ebc1fb6071d2be8098153b863
AlexShpigun <alexshpigun0@gmail.com>
2023-01-26 19:49:27 +0200

    I've created ManagerCustomerPage

--- src/main/java/pages/ManagerCustomersPage.java
+++ src/main/java/pages/ManagerCustomersPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import basePages.AbstractBasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ManagerCustomersPage extends AbstractBasePage {
 
@@ -11,7 +12,49 @@ public class ManagerCustomersPage extends AbstractBasePage {
     }
 
     @Override
-    public <T> T open() {
-        return null;
+    public ManagerCustomersPage open() {
+        openUrl(env + "manager");
+        return this;
+    }
+
+    private static final String FIRST_NAME = "//a[contains(text() , 'First Name')]";
+    private static final String LAST_NAME = "//a[contains(text() , 'Last Name')]";
+    private static final String POST_CODE = "//a[contains(text() , 'Post Code')]";
+    private static final String ACCOUNT_NUMBER = "//a[contains(text() , 'Account Number')]";
+    private static final String DELETE_CUSTOMER = "//a[contains(text() , 'Delete Customer')]";
+
+    public WebElement firstCustomerName() {
+        return waitVisibleOfElement(FIRST_NAME);
+    }
+    public WebElement lastCustomerName() {
+        return waitVisibleOfElement(LAST_NAME);
+    }
+
+    public WebElement postCode() {
+        return waitVisibleOfElement(POST_CODE);
+    }
+    public WebElement accNumber() {
+        return waitVisibleOfElement(ACCOUNT_NUMBER);
+    }
+
+
+    public ManagerCustomersPage searchCustomerByFirstName(String name) {
+            sendText(firstCustomerName(), name);
+            return this;
+        }
+
+
+    public ManagerCustomersPage searchCustomerByLastName(String name1) {
+        sendText(lastCustomerName(), name1);
+        return this;
+    }
+    public ManagerCustomersPage searchCustomerByPostIndex(String postIndex) {
+        sendText(postCode(), postIndex);
+        return this;
+    }
+
+    public WebElement delCustomer() {
+        return waitVisibleOfElement(DELETE_CUSTOMER);
     }
 }
+
