1e611f9cc1d6349a35a7cce837ab857ef74e97a2
TrofDanil <trofimov.danil.sergiovich@gmail.com>
2023-01-30 15:29:00 +0200

    implement customer auth test

--- src/test/java/tests/TestCustomerAuthirization.java
+++ src/test/java/tests/TestCustomerAuthirization.java
@@ -8,22 +8,29 @@ import org.testng.annotations.Test;
 
 import pages.CustomerLoginPage;
 import pages.CustomerPage;
-import pages.ManagerOpenAccountPage;
 
 @Log4j
 public class TestCustomerAuthirization extends TestInit {
 
     CustomerLoginPage customerLoginPage;
     CustomerPage customerPage;
-
+    String USER = "Harry Potter";
     @BeforeTest
     public void setup() {
         customerLoginPage = new CustomerLoginPage(driver);
-        customerPage = new CustomerPage(driver);
     }
     @Test
     public void testAuthirization() {
-        
         log.info("--------------------go to Customer Login Page--------------------");
+        customerLoginPage.open();
+        log.info("--------------------check user--------------------");
+        //
+        log.info(String.format("--------------------select user %s--------------------", USER));
+        customerLoginPage.selectUser(USER);
+        Assert.assertTrue(customerLoginPage.loginBtn().isDisplayed(), "Login button is not displayed");
+        log.info("--------------------login--------------------");
+        customerPage = customerLoginPage.clickLoginBtn();
+        log.info("--------------------verify login--------------------");
+        Assert.assertEquals(customerPage.getName(), USER, "The login does not match"); 
     }
 }
